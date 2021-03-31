f213465b3b7e714d6ecb2dc3a59979ed302c4dfd
Igor Cananea <icc@avalonbits.com>
2021-03-31 16:33:45 -0700

    Added the sqlite3 schema.

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS RequestCache(
+    Endpoint TEXT NOT NULL,
+    Params TEXT NOT NULL,
+    Timestamp TEXT NOT NULL,
+    Response BLOB NOT NULL,
+
+    PRIMARY KEY (Endpoint, Params, Timestamp)
+);
+CREATE INDEX EndpointIDX ON RequestCache(Endpoint);
+CREATE INDEX OrderedEndpointIDX ON RequestCache(Endpoint, Timestamp);
