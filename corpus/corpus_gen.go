a1a6659047d6f1d1b28fe1135225139805b1047f
Igor Cananea <icc@avalonbits.com>
2021-04-04 13:39:04 -0700

    Add GPLv3 license to generated code.

--- corpus/corpus_gen.go
+++ corpus/corpus_gen.go
@@ -2,6 +2,24 @@
 // Any changes will be lost if this file is regenerated.
 // see https://github.com/cheekybits/genny
 
+/*
+ * Copyright (C) 2021  Igor Cananea <icc@avalonbits.com>
+ * Author: Igor Cananea <icc@avalonbits.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package corpus
 
 import (
@@ -52,6 +70,24 @@ func (c Corpus) getFballCountryResponse(
 
 	return []fball.CountryResponse{crQ}, nil
 }
+
+/*
+ * Copyright (C) 2021  Igor Cananea <icc@avalonbits.com>
+ * Author: Igor Cananea <icc@avalonbits.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 func (c Corpus) getFballTimezoneResponse(
 	ctx context.Context, endpoint string, max int, rng db.Range, policy refreshPolicy,
